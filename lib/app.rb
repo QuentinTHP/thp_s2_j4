88d56306146e824f60f1e640f297935804f13999
QuentinTHP <quentin.clere.pro@gmail.com>
2019-01-17 15:33:32 +0100

    Trader de l'obscur finish

--- lib/app.rb
+++ lib/app.rb
@@ -1,14 +1,51 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-PAGE_URL = "http://www.google.com/search?q=doughnuts"
+require 'pry'
+PAGE_URL = "https://coinmarketcap.com/all/views/all/"
+
+puts "\e[H\e[2J"    #clear le terminal
+
+
+$doc = Nokogiri::HTML(open(PAGE_URL))
+
+def crypto_name()
+  array_name = []
+  $doc.xpath('//td[3]').each do |symbol|
+    symbol.text 
+    array_name << symbol.text 
+    end
+  return array_name
+end
+
+
+def crypto_values()
+    array_values = []
+    $doc.xpath('//td[5]/a').each do |values|
+      values.text 
+      array_values << values.text
+      end
+  return array_values
+end
+
+
+def crypto_names_and_values()
+  puts hash_names_values = Hash[crypto_name().zip crypto_values()]
+end
+
+crypto_names_and_values()
 
 
 
-page = Nokogiri::HTML(open(PAGE_URL))
 
+# Ensemble des crypto : 
+ #   //*[@id="currencies-all"]/tbody
 
-doc = Nokogiri::HTML(open(PAGE_URL))
-	doc.xpath('//h3/a').each do |node|
-	  puts node.text
-	end
+# Ligne du BTC :
+  #  //*[@id="id-bitcoin"]
+
+# Contient le symbol BTC :
+ #   //*[@id="id-bitcoin"]/td[3]
+
+# Prix du BTC :
+ #   //*[@id="id-bitcoin"]/td[5]
